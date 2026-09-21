8d2ef0fc977009ee5e815e8e4c2e7a4b26de1c66
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-09-21 20:53:09 +0200

    Actually fix GitPushCommand

--- Kavita.ReleaseTools/Commands/Git/GitPushCommand.cs
+++ Kavita.ReleaseTools/Commands/Git/GitPushCommand.cs
@@ -26,7 +26,7 @@ public class GitPushCommand : MutatingCommand
     protected override Task ExecuteAsync(ExecutionContext ctx, CancellationToken ct)
     {
         var pushOptions = new PushOptions { CredentialsProvider = (_, _, _) => new UsernamePasswordCredentials
-            { Username = _authToken, Password = string.Empty } // Password needs to be set as empty. Throws on NULL
+            { Username = "x-auth-token", Password = _authToken }
         };
 
         var remote = ctx.Git.Repository.Network.Remotes[_remote];
